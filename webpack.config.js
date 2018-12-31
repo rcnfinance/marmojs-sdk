3a7079420e693546c7f64254ae7af56a108f335a
Joaquin Gonzalez <jpgonzalezra@gmail.com>
2018-12-31 20:08:21 -0300

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,9 @@
-const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const nodeEnv = process.env.NODE_ENV || 'development';
-const isProd = nodeEnv === 'production';
-
-const plugins = [
-  new webpack.DefinePlugin({
-    'process.env': {
-      NODE_ENV: JSON.stringify(nodeEnv)
-    }
-  }),
-  new HtmlWebpackPlugin({
-    title: 'Typescript Webpack Starter',
-    template: '!!ejs-loader!src/index.html'
-  }),
-  new webpack.LoaderOptionsPlugin({
-    options: {
-      tslint: {
-        emitErrors: true,
-        failOnHint: true
-      }
-    }
-  })
-];
 
 var config = {
-  devtool: isProd ? 'hidden-source-map' : 'source-map',
+  mode: 'production',
+  devtool: 'hidden-source-map',
   context: path.resolve('./src'),
   entry: {
     app: './index.ts'
@@ -41,25 +19,15 @@ var config = {
         test: /\.tsx?$/,
         exclude: [/\/node_modules\//],
         use: ['awesome-typescript-loader', 'source-map-loader']
-      },
-      !isProd
-        ? {
-            test: /\.(js|ts)$/,
-            loader: 'istanbul-instrumenter-loader',
-            exclude: [/\/node_modules\//],
-            query: {
-              esModules: true
-            }
-          }
-        : null,
-      { test: /\.html$/, loader: 'html-loader' },
-      { test: /\.css$/, loaders: ['style-loader', 'css-loader'] }
+      }
     ].filter(Boolean)
   },
   resolve: {
     extensions: ['.ts', '.js']
   },
-  plugins: plugins,
+  plugins: [
+    new CleanWebpackPlugin(['dist'])
+  ],
   devServer: {
     contentBase: path.join(__dirname, 'dist/'),
     compress: true,
