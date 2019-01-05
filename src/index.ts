8c7e9f74d69e7f0bea75e165b0b589cc797ec319
Joaquin Gonzalez <jpgonzalezra@gmail.com>
2019-01-04 23:39:14 -0300

    [ADD] public exports

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import { IntentBuilder } from './builder/IntentBuilder';
+import { IntentAction } from './model/IntentAction';
+import { Intent } from './model/Intent';
+import { IntentRequest } from './model/request/IntentRequest';
+import * as MarmoUtils from './utils/MarmoUtils';
+// import { RelayClient } from './client/RelayClient'
+
+export {
+    IntentBuilder,
+    IntentAction,
+    Intent,
+    IntentRequest,
+    MarmoUtils
+    // RelayClient
+};
