cb2a5cc7c42a6845257f81c5439acde86b5a2ea5
Joaquin Gonzalez <jpgonzalezra@gmail.com>
2019-01-05 00:49:12 -0300

    [ADD ]new exports

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { IntentBuilder } from './builder/IntentBuilder';
 import { IntentAction } from './model/IntentAction';
 import { Intent } from './model/Intent';
 import { IntentRequest } from './model/request/IntentRequest';
-import * as MarmoUtils from './utils/MarmoUtils';
+import { generateAddress, toHexStringZeroPadded, toHexStringNoPrefixZeroPadded, sign, encodeDataPayload } from './utils/MarmoUtils';
 // import { RelayClient } from './client/RelayClient'
 
 export {
@@ -10,6 +10,10 @@ export {
     IntentAction,
     Intent,
     IntentRequest,
-    MarmoUtils
+    generateAddress,
+    toHexStringZeroPadded,
+    toHexStringNoPrefixZeroPadded,
+    sign,
+    encodeDataPayload
     // RelayClient
 };
