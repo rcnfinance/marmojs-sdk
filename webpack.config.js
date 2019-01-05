c41e43991f46c0b02d9e26ca658a0d7becbc55b0
Joaquin Gonzalez <jpgonzalezra@gmail.com>
2019-01-04 23:38:18 -0300

    [CONFIG] plugin in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './src/builder/IntentBuilder.ts',
+  entry: './src/index.ts',
   module: {
     rules: [
       {
@@ -11,13 +12,17 @@ module.exports = {
       }
     ]
   },
+  devtool: 'source-map',
   resolve: {
     extensions: [ '.tsx', '.ts', '.js' ]
   },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: path.join('[name]', 'index.ts'),
+    filename: 'index.ts',
     library: "marmojs-sdk",
     libraryTarget: "umd"
-  }
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist'])
+  ]
 };
