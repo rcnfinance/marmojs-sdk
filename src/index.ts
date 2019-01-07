42242426bf24d25e32f4d55b2f8ad622b150314e
Joaquin Gonzalez <jpgonzalezra@gmail.com>
2019-01-06 22:26:42 -0300

    [ADD] export function

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { IntentBuilder } from './builder/IntentBuilder';
 import { IntentAction } from './model/IntentAction';
 import { Intent } from './model/Intent';
 import { IntentRequest } from './model/request/IntentRequest';
-import { generateAddress, toHexStringZeroPadded, toHexStringNoPrefixZeroPadded, sign, encodeDataPayload } from './utils/MarmoUtils';
+import { generateAddress, toHexStringZeroPadded, toHexStringNoPrefixZeroPadded, sign, encodeDataPayload, transformSignedIntent } from './utils/MarmoUtils';
 // import { RelayClient } from './client/RelayClient'
 
 export {
@@ -14,6 +14,7 @@ export {
     toHexStringZeroPadded,
     toHexStringNoPrefixZeroPadded,
     sign,
-    encodeDataPayload
+    encodeDataPayload,
+    transformSignedIntent
     // RelayClient
 };
