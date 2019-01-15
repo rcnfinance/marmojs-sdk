fedbb8c835c410fb555120f2442eaa9e35be46a3
Joaquin Gonzalez <jpgonzalezra@gmail.com>
2019-01-15 13:27:13 -0300

    [ADD] Event Route and script for test.-

--- src/event/EventInit.ts
+++
@@ -1,9 +0,0 @@
-import ConfigurationService from "./service/EventConfiguration";
-
-const configurationService = new ConfigurationService()
-
-async function run() {
-  await configurationService.startWatching()
-}
-
-run().catch(console.log)
---
+++ src/event/EventRoute.ts
@@ -0,0 +1,28 @@
+import { EventService }  from './service/eventService';
+import { ConfigurationService } from "./service/EventConfiguration";
+
+export class EventRoute {
+
+  private eventService: EventService
+  private configurationService: ConfigurationService
+
+  constructor() {
+    this.configurationService = new ConfigurationService()
+    this.eventService = this.configurationService.eventService;
+  }
+
+  public async run() {
+    await this.configurationService.startWatching()
+  }
+
+  public init() {
+    this.run().catch(console.log)
+    console.log('---------')
+  }
+
+  public addEvent(event) {
+    this.eventService.storeNewEvent(event)
+  }
+
+}
+
