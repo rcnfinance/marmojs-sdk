7da67d726528948ca3807dfb48b4000f78a09b24
Agustin <agusxrun@gmail.com>
2019-01-28 17:49:38 +0100

    Export DefaultConf and Conf

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,14 @@ import { IntentBuilder } from './builder/IntentBuilder';
 import { Intent } from './model/Intent';
 import { RelayClient } from './client/RelayClient'
 import { IntentAction } from './model/IntentAction';
+import { Config, DefaultConf } from './Config';
+
 
 export {
     IntentBuilder,
     IntentAction,
     Intent,
-    RelayClient
+    RelayClient,
+    DefaultConf,
+    Config
 };
