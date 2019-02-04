73529451702d4fcab96e45f9a49a807f736e9916
Agustin <agusxrun@gmail.com>
2019-02-04 17:16:28 +0100

    Add staticcall dependency

--- src/Config.ts
+++ src/Config.ts
@@ -3,13 +3,16 @@ let globalConf: Config;
 export class Config {
     initCode: string;
     marmoFactory: string;
+    depsUtils: string;
 
     constructor(
         initCode: string,
         marmoFactory: string,
+        depsUtils: string
     ) {
         this.initCode = initCode;
         this.marmoFactory = marmoFactory;
+        this.depsUtils = depsUtils;
     }
 
     asDefault() {
@@ -22,5 +25,9 @@ export class Config {
 }
 
 export class DefaultConf {
-    static ROPSTEN = new Config("0x98ef25e9f596000233ed019f909cc8a5f35984f1cc0b0b9e05407ce7a6820bc1", "0x6306b6a26c70c03279c037f630be03046104cb37");
+    static ROPSTEN = new Config(
+        "0x98ef25e9f596000233ed019f909cc8a5f35984f1cc0b0b9e05407ce7a6820bc1",
+        "0x6306b6a26c70c03279c037f630be03046104cb37",
+        "0x874ad09c8ab7da34bf75550409e9446b47558364"
+    );
 }
--- src/model/Intent.ts
+++ src/model/Intent.ts
@@ -1,11 +1,26 @@
 import { Wallet } from "src/model/Wallet";
 import { IntentAction } from "src/model/IntentAction";
 import BigNumber = require("bn.js");
+import { SignedIntent } from "./SignedIntent";
+import { Config } from "src/Config";
 
 const Web3 = require('web3');
 
+export class IntentDependency {
+    address: string;
+    id: string;
+
+    constructor(
+        address: string,
+        id: string
+    ) {
+        this.address = address;
+        this.id = id;
+    }
+}
+
 export class Intent {
-    dependencies: string[];
+    dependencies: IntentDependency[];
     action: IntentAction;
     salt: string;
     maxGasPrice: BigNumber;
@@ -13,19 +28,32 @@ export class Intent {
     expiration: BigNumber;
 
     constructor(
-        dependencies: string[],
+        dependencies: Array<IntentDependency | SignedIntent>,
         action: IntentAction,
         salt: string,
         maxGasPrice: BigNumber,
         minGasLimit: BigNumber,
         expiration: BigNumber
     ) {
-        this.dependencies = dependencies;
+        this.dependencies = [];
         this.action = action;
         this.salt = salt;
         this.maxGasPrice = maxGasPrice;
         this.minGasLimit = minGasLimit;
         this.expiration = expiration;
+
+        dependencies.forEach(d => this.add_dependency(d));
+    }
+
+    add_dependency(dependency: IntentDependency | SignedIntent) {
+        if (dependency instanceof IntentDependency) {
+            this.dependencies.push(dependency);
+        } else {
+            this.dependencies.push(new IntentDependency(
+                dependency.wallet.address,
+                dependency.id
+            ));
+        }
     }
 
     id(wallet: Wallet): string {
@@ -42,7 +70,7 @@ export class Intent {
         if (this.dependencies.length === 0) {
             depsHash = emptyHash;
         } else {
-            depsHash = Web3.utils.soliditySha3({ t: 'bytes32[]', v: this.dependencies });
+            depsHash = Web3.utils.soliditySha3({ t: 'bytes', v: this.build_dependency_call(wallet.config) });
         }
 
         return Web3.utils.soliditySha3(
@@ -56,5 +84,44 @@ export class Intent {
             { t: 'bytes32', v: this.salt },
             { t: 'uint256', v: this.expiration }
         );
+    }
+
+    build_dependency_call(config: Config): string {
+        const depsCount = this.dependencies.length
+
+        if (depsCount == 0) {
+            // No dependencies
+            return "0x";
+        } else if (depsCount == 1) {
+            // Single dependency, call wallet directly
+            const call = Web3.eth.abi.encodeFunctionCall({
+                name: 'relayedAt',
+                type: 'function',
+                inputs: [{
+                    type: 'bytes32',
+                    name: '_id'
+                }]
+            }, [this.dependencies[0].id]);
+
+            return "0x" + this.dependencies[0].address.replace('0x', '') + call.replace('0x', '');
+        } else {
+            // Multiple dependencies, using DepsUtils contract
+            const call = Web3.eth.abi.encodeFunctionCall({
+                name: 'multipleDeps',
+                type: 'function',
+                inputs: [{
+                    type: 'address[]',
+                    name: '_wallets'
+                },{
+                    type: 'bytes32[]',
+                    name: '_ids'
+                }]
+            }, [
+                this.dependencies.map(d => d.address),
+                this.dependencies.map(d => d.id)
+            ]);
+
+            return "0x" + config.depsUtils.replace('0x', '') + call.replace('0x', '');
+        }
     }
 }
