487fa8617f2dcebf9020b36aa3cebe8f69376db9
Joaquin Gonzalez <jpgonzalezra@gmail.com>
2019-02-15 23:10:55 -0300

    fix merge

--- src/model/data/EthWallet.ts
+++ src/model/data/EthWallet.ts
@@ -4,7 +4,7 @@ import BigNumber = require("bn.js");
 import { Contract } from "./Contract";
 import { Function } from "./Function";
 
-export class EthWallet extends Contract {
+export class EthWallet {
 
     sendEth(to: string, value: number | string | BigNumber): IntentAction {
         return new IntentAction(to, new BigNumber(value), "0x", new Function())
