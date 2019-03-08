69a3473aacef9f7f46b43302b8430b20e11e52e6
Joaquin Gonzalez <jpgonzalezra@gmail.com>
2019-03-08 20:32:28 -0300

    [FIX]

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import { IntentBuilder } from './builder/IntentBuilder';
 import { Intent } from './model/Intent';
+import { SignedIntent } from './model/SignedIntent';
 import { RelayClient } from './client/RelayClient'
 import { IntentAction } from './model/IntentAction';
 import { Config, DefaultConf } from './Config';
 import { Provider } from './Provider';
+import { Wallet } from './model/Wallet';
+import { ERC20 } from './model/data/ERC20';
+import { EthWallet } from './model/data/EthWallet';
+import { WETH } from './model/data/WETH';
+import { StatusCode } from './model/response/Status';
 
 export {
     IntentBuilder,
@@ -12,5 +18,11 @@ export {
     RelayClient,
     DefaultConf,
     Config,
-    Provider
+    Provider,
+    Wallet,
+    ERC20,
+    EthWallet,
+    WETH,
+    SignedIntent,
+    StatusCode
 };
