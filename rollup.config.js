169dc54978d4ffd48c2d52647a03a601988101fb
Joaquin Gonzalez <jpgonzalezra@gmail.com>
2019-07-01 15:15:08 -0300

    enhances

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,11 @@
-import typescript from 'rollup-plugin-typescript';
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
 import pkg from './package.json';
+
+const extensions = [
+    '.js', '.jsx', '.ts', '.tsx',
+];
 
 const name = 'RollupTypeScriptBabel';
 
@@ -8,10 +14,17 @@ export default {
 
     // Specify here external modules which you don't want to include in your bundle (for instance: 'lodash', 'moment' etc.)
     // https://rollupjs.org/guide/en#external-e-external
-    external: [],
+    external: [ ],
 
     plugins: [
-        typescript(),
+        // Allows node_modules resolution
+        resolve({ extensions }),
+
+        // Allow bundling cjs modules. Rollup doesn't understand cjs
+        commonjs(),
+
+        // Compile TypeScript/JavaScript files
+        babel({ extensions, include: ['src/**/*'] }),
     ],
 
     output: [{
